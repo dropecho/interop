92df03f9fc0a71fd2a59535a542671050f8d10b4
Benjamin Van Treese <vantreeseba@gmail.com>
2022-11-30 17:17:43 -0600

    feat: Remove abstract func tests for now.

--- test/interop/AbstractFuncTest.hx
+++ test/interop/AbstractFuncTest.hx
@@ -10,41 +10,41 @@ class Whatever {
 }
 
 class AbstractFuncTest {
-	macro private static function testMacro() {
-		var foo = "hello";
-
-		return macro trace(foo);
-	}
-
-	@Test
-	public function can_instantiate() {
-		var m:Action_0 = () -> trace("hi");
-		Assert.isNotNull(m);
-	}
-
-	@Test
-	public function action0() {
-		var val = 0;
-		var m:Action_0 = () -> val += 1;
-		m.call();
-
-		// testMacro();
-	}
-
-	@Test
-	public function action1() {
-		var val = 0;
-		var m:Action_1<Int> = (p1:Int) -> val += 1;
-		m.call(1);
-	}
-
-	@Test
-	public function func2() {
-		var m = (a, b) -> a + b;
-		var foo = new Whatever();
-		foo.func2 = m;
-		var r = foo.func2.call(1, 2);
-
-		Assert.areEqual(r, 3);
-	}
+	//   macro private static function testMacro() {
+	//     var foo = "hello";
+	//
+	//     return macro trace(foo);
+	//   }
+	//
+	//   @Test
+	//   public function can_instantiate() {
+	//     var m:Action_0 = () -> trace("hi");
+	//     Assert.isNotNull(m);
+	//   }
+	//
+	//   @Test
+	//   public function action0() {
+	//     var val = 0;
+	//     var m:Action_0 = () -> val += 1;
+	//     m.call();
+	//
+	//     // testMacro();
+	//   }
+	//
+	//   @Test
+	//   public function action1() {
+	//     var val = 0;
+	//     var m:Action_1<Int> = (p1:Int) -> val += 1;
+	//     m.call(1);
+	//   }
+	//
+	//   @Test
+	//   public function func2() {
+	//     var m = (a, b) -> a + b;
+	//     var foo = new Whatever();
+	//     foo.func2 = m;
+	//     var r = foo.func2.call(1, 2);
+	//
+	//     Assert.areEqual(r, 3);
+	//   }
 }
